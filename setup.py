39cafa3c9d260aa3deb10a99225ffa0e8f925c16
0phoff <0phoff@users.noreply.github.com>
2017-10-04 15:08:43 +0200

    Created Pascal VOC to darknet script

--- setup.py
+++ setup.py
@@ -4,5 +4,11 @@ setup(name='brambox',
       version='0.0.1',
       description='Unified tools for generating PR curves, crunshing image data annotation sets and more',
       author='EAVISE',
-      packages=['brambox', 'brambox.annotations', 'brambox.transforms'],
-      scripts=['scripts/format_vatic_to_dollar.py', 'scripts/show_annotations.py', 'scripts/swap_image_channel.py'])
+      packages=['brambox',
+                'brambox.annotations',
+                'brambox.transforms'],
+
+      scripts=[ 'scripts/format_vatic_to_dollar.py',
+                'scripts/format_pascalvoc_to_darknet.py',
+                'scripts/show_annotations.py',
+                'scripts/swap_image_channel.py'])
