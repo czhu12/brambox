c17f0d39549ef7e262e05f1cf072c6c2b8a4edab
Maarten Vandersteegen <maarten.vandersteegen@kuleuven.be>
2017-11-02 09:19:17 +0100

    Refactored expand function
    Quickly tested expand, not all paths covered
    Still need to write unit tests

--- brambox/annotations/util/path.py
+++ brambox/annotations/util/path.py
@@ -4,16 +4,44 @@
 #
 
 import os
+import glob
 
-__all__ = ["files", "expand"]
+__all__ = ["files", "strider", "expand"]
 
 
 def files(path):
-    """ list all files in a directory """
+    """ list all files in a directory omitting directories """
     for file in os.listdir(path):
         if os.path.isfile(os.path.join(path, file)):
             yield os.path.join(path, file)
 
+def strider(elements, stride, offset):
+    """ yield input elements with given stride and offset """
+    next_element = offset
+
+    # support negative offsets
+    while next_element < 0:
+        next_element += stride
+
+    for i, elem in enumerate(elements):
+        if i == next_element:
+            next_element += stride
+            yield elem
+
+def modulo_expand(expr, stride, offset):
+    """ """
+    # TODO: refactor this
+    very_big_number = 10000000000
+    for file_number in range(offset, very_big_number, stride):
+        if file_number < 0:
+            continue
+
+        filename = expr % file_number
+
+        if not os.path.isfile(filename):
+            break
+
+        yield filename
 
 def expand(expr, stride=1, offset=0):
     """Expand a file selection expression into multiple filenames
@@ -22,7 +50,7 @@ def expand(expr, stride=1, offset=0):
         * a file itself (just return the filename)
         * a directory
         * a filename containing %d
-    This function will yield all filenames found matching this expression
+    This function will return a generator object that produces full filesnames
 
     expr   -- file sequence expression
     stride -- sample every n'th file where n is this parameter
@@ -30,33 +58,16 @@ def expand(expr, stride=1, offset=0):
     """
 
     if os.path.isdir(expr):
-        next_file_number = offset
-
-        # support negative offsets
-        while next_file_number < 0:
-            next_file_number += stride
-
-        for i, file in enumerate(sorted(files(expr))):
-            if i == next_file_number:
-                next_file_number += stride
-                yield file
+        return strider(sorted(files(expr)), stride, offset)
 
     elif os.path.isfile(expr):
-        yield expr
+        return expr
+
+    elif '*' in expr:
+        return strider(sorted(glob.glob(expr)), stride, offset)
 
     elif '%' in expr:
-        # TODO: refactor this
-        very_big_number = 10000000000
-        for file_number in range(offset, very_big_number, stride):
-            if file_number < 0:
-                continue
-
-            filename = expr % file_number
-
-            if not os.path.isfile(filename):
-                break
-
-            yield filename
+        return modulo_expand(expr, stride, offset)
 
     else:
         raise TypeError("File selection expression invalid")
