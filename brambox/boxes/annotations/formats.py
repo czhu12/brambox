616e8d072225bbfa7b0fce77298488420c9401c4
0phoff <tanguy.ophoff@kuleuven.be>
2017-12-14 13:37:22 +0100

    Added pascalvoc annotation parser to formats dictionary

--- brambox/boxes/annotations/formats.py
+++ brambox/boxes/annotations/formats.py
@@ -18,6 +18,7 @@ annotation_formats = {
     'darknet': DarknetParser,
     'dollar': DollarParser,
     'pickle': PickleParser,
+    'pascalvoc': PascalVOCParser,
     'vatic': VaticParser,
     'yaml': YamlParser,
 }
