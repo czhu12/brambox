d65f4eb5227dad0b2bb0a72481af70c267b2f9a0
0phoff <tanguy.ophoff@kuleuven.be>
2017-12-27 18:33:31 +0100

    Added API docs for brambox.transforms

--- brambox/transforms/channelMixer.py
+++ brambox/transforms/channelMixer.py
@@ -10,20 +10,40 @@ __all__ = ['ChannelMixer']
 
 
 class ChannelMixer:
-    """ Mix channels of multiple inputs in a single output image """
+    """ Mix channels of multiple inputs in a single output image.
+    This class works with opencv_ images (np.ndarray), and will mix the channels of multiple images into one new image.
 
+    Args:
+        num_channels (int, optional): The number of channels the output image will have; Default **3**
+
+    Example:
+        >>> # Replace the 3th channel of an image with a channel from another image
+        >>> mixer = brambox.transforms.ChannelMixer()
+        >>> mixer.set_channels([(0,0), (0,1), (1,0)])
+        >>> out = mixer(img1, img2)
+        >>> # out => opencv image with channels: [img0_channel0, img0_channel1, img1_channel0]
+    """
     def __init__(self, num_channels=3):
         self.num_channels = num_channels
         self.channels = [(0, i) for i in range(num_channels)]
 
     def set_channels(self, channels):
-        """ Set from which channels the output image should be created [list with (imageNumber, channelNumber) tupples] """
+        """ Set from which channels the output image should be created.
+        The channels list should have the same length as the number of output channels.
+
+        Args:
+            channels (list): List of tuples containing (img_number, channel_number)
+        """
         if len(channels) != self.num_channels:
             raise ValueError('You should have one [image,channel] per output channel')
         self.channels = [(c[0], c[1]) for c in channels]
 
     def __call__(self, *imgs):
-        """ Compute and return output image """
+        """ Create and return output image.
+
+        Args:
+            *imgs: Argument list with all the images needed for the mix
+        """
         m = max(self.channels, key=lambda c: c[0])[0]
         if m >= len(imgs):
             raise ValueError(f'{m} images are needed to perform the mix')
---
+++ docs/links.rst
@@ -0,0 +1,11 @@
+.. LINKS
+.. _brambox: https://www.gitlab.com/EAVISE/brambox/
+.. _python: https://docs.python.org/3.6/
+.. _darknet: https://pjreddie.com/darknet
+.. _pascal voc: http://host.robots.ox.ac.uk/pascal/VOC/
+.. _opencv: https://opencv.org/
+
+.. DIRECTIVES
+.. |br| raw:: html
+
+   <br />
